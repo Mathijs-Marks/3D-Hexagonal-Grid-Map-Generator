78ce283db010d3920f10524499356e244a88a2cb
Mathijs Marks <mathijsmarks@msn.com>
2022-03-14 11:54:25 +0100

    Coordinates of each cell in the grid are now shown.

--- Procedural Hex Map Generator/Assets/Scripts/HexGrid.cs
+++ Procedural Hex Map Generator/Assets/Scripts/HexGrid.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Create Hex grid component composed of Hex cell prefabs.
@@ -13,14 +14,19 @@ public class HexGrid : MonoBehaviour
     public int height = 6;
 
     public HexCell cellPrefab;
-
     private HexCell[] cells;
+
+    public Text cellLabelPrefab;
+    private Canvas gridCanvas;
 
     /// <summary>
     /// Determine the size of the cell grid and fill with cells.
+    /// Visualize cell coordinates using a canvas.
     /// </summary>
     private void Awake()
     {
+        gridCanvas = GetComponentInChildren<Canvas>();
+
         cells = new HexCell[height * width];
 
         for (int z = 0, i = 0; z < height; z++)
@@ -35,6 +41,7 @@ public class HexGrid : MonoBehaviour
     /// <summary>
     /// Each cell created will have their xz sizes equal 10, y size is zero to make it lay flat.
     /// Position the cell on the grid accordingly.
+    /// For each cell, render a UI text element, showing the coordinates of the cell.
     /// </summary>
     /// <param name="x"></param>
     /// <param name="z"></param>
@@ -49,5 +56,11 @@ public class HexGrid : MonoBehaviour
         HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
         cell.transform.SetParent(transform, false);
         cell.transform.localPosition = position;
+
+        Text label = Instantiate<Text>(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition =
+            new Vector2(position.x, position.z);
+        label.text = x.ToString() + "\n" + z.ToString();
     }
 }
